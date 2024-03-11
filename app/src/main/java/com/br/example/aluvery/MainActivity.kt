884f4c855101db5c2df17da9ef603dd2e33e2d7f
Karen Jully <karenjully.kr15@gmail.com>
2024-03-11 11:43:45 -0300

    test: modifier

--- app/src/main/java/com/br/example/aluvery/MainActivity.kt
+++ app/src/main/java/com/br/example/aluvery/MainActivity.kt
@@ -3,13 +3,20 @@ package com.br.example.aluvery
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.br.example.aluvery.ui.theme.AluveryTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,50 +25,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             AluveryTheme {
                 Surface {
-                    MyFirstComposable()
+                    CustomPreview()
                 }
-            }
-        }
-    }
-}
-
-@Composable
-fun MyFirstComposable() {
-    Text(text = "texto 1")
-    Text(text = "texto 2")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun ColumnPreview() {
-    AluveryTheme {
-        Surface {
-            Column {
-                MyFirstComposable()
-            }
-        }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun RowPreview() {
-    AluveryTheme {
-        Surface {
-            Row {
-                MyFirstComposable()
-            }
-        }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun BoxPreview() {
-    AluveryTheme {
-        Surface {
-            Box {
-                MyFirstComposable()
             }
         }
     }
@@ -72,19 +37,41 @@ fun BoxPreview() {
 fun CustomPreview() {
     AluveryTheme {
         Surface {
-            Column {
+            Column(
+                Modifier
+                    .padding(8.dp)
+                    .background(Color.Blue)
+                    .padding(8.dp)
+                    .fillMaxSize()
+            ) {
                 Text(text = "texto 1")
                 Text(text = "texto 2")
-                Row {
+                Row(
+                    Modifier
+                        .fillMaxWidth(0.9f)
+                        .padding(horizontal = 8.dp, vertical = 16.dp)
+                        .background(Color.Green)
+                ) {
                     Text(text = "texto 3")
                     Text(text = "texto 4")
                 }
-                Box {
-                    Row {
+                Box(
+                    Modifier
+                        .background(Color.Red)
+                        .padding(16.dp)
+                ) {
+                    Row(
+                        Modifier
+                            .fillMaxWidth()
+                            .background(Color.Cyan)
+                    ) {
                         Text(text = "texto 5")
                         Text(text = "texto 6")
                     }
-                    Column {
+                    Column(
+                        Modifier
+                            .background(Color.Yellow)
+                    ) {
                         Text(text = "texto 7")
                         Text(text = "texto 8")
                     }
